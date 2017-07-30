8776140b85d5db40bed4131d4ca7eadadd954ce6
Dmitry Legchikov <legchikov@dmitry.by>
2017-07-30 10:58:33 +0300

    resolve week-1 task-1

--- python/week-1/task_1.py
+++ python/week-1/task_1.py
@@ -1,26 +1,29 @@
 # Task 1
 
 def check_brackets(seq):
-    arr = []
-    count = 0
-    for count, x in enumerate(seq):
+    stack = []
+    error_stack = []
+
+    for count, x in enumerate(seq, 1):
         if x in '([{':
-            arr.append(x)
+            stack.append(x)
+            error_stack.append(count)
         if x in ')]}':
-            if arr:
-                last = arr.pop()
+            if stack:
+                last = stack.pop()
+                last_error = error_stack.pop()
                 if x == ')':
                     if last != '(':
-                        return str(count+1)
+                        return str(count)
                 if x == ']':
                     if last != '[':
-                        return str(count+1)
+                        return str(count)
                 if x == '}':
                     if last != '{':
-                        return str(count+1)
+                        return str(count)
             else:
-                return str(count+1)
-    if arr:
-        return str(count+1)
+                return str(count)
+    if error_stack:
+        return str(error_stack.pop())
 
     return 'Success'
