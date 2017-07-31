bf94d6495439dfa050e9222f56e5cab1bee3a346
Dmitry Legchikov <legchikov@dmitry.by>
2017-07-31 08:19:27 +0300

    edit week-1 task-2

--- python/week-1/task_2.py
+++ python/week-1/task_2.py
@@ -1,11 +1,6 @@
 # Task 2
 
-def height_tree(num, seq):
-    i = 0
-    while num:
-        print(seq[i])
-        num -= 1
-        i += 1
+def height_tree(seq):
+    parents = [seq[i] for i in range(len(seq))]
 
-arr = [1, 2, 3, 4, 5, 6, 7]
-height_tree(3, arr)
+height_tree([4, -1, 4, 1, 1])
--- python/week-1/task_2_test.py
+++ python/week-1/task_2_test.py
@@ -5,7 +5,12 @@ class HeightTreeTestCase(unittest.TestCase):
     """Tests for `task_2.py`."""
 
     def test_1(self):
-        self.assertEqual('Success', height_tree())
+        self.assertEqual('3', height_tree([4, -1, 4, 1, 1]))
+
+
+    def test_2(self):
+        self.assertEqual('4', height_tree([-1, 0, 4, 0, 3]))
+
 
 if __name__ == '__main__':
     unittest.main()
