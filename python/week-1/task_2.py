398873b495be9a6243b554438fb18159e252b8c4
Dmitry Legchikov <legchikov@dmitry.by>
2017-08-03 08:31:42 +0300

    edit week-1 task-2

--- python/week-1/task_2.py
+++ python/week-1/task_2.py
@@ -1,6 +1,31 @@
 # Task 2
 
-def height_tree(seq):
-    parents = [seq[i] for i in range(len(seq))]
+def height_tree(unique_parents, node, count):
+    if unique_parents:
+        #print(unique_parents)
+        item = unique_parents.pop(node)
+        print(item)
+        count += 1
+        height_tree(unique_parents, item, count)
+    else:
+        return count
 
-height_tree([4, -1, 4, 1, 1])
+
+
+
+
+seq = [4, -1, 4, 1, 1]
+seq = [-1, 0, 4, 0, 3]
+parents = [seq[i] for i in range(len(seq))]
+print(parents)
+
+root = 0
+for i, x in enumerate(seq):
+    if x == -1:
+        root = i
+print(root)
+unique_parents = {parent:node for node, parent in enumerate(parents)}
+print(unique_parents)
+height = height_tree(unique_parents, -1, 0)
+print(height)
+#print(unique_parents)
